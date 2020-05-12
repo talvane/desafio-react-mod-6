f0c3089fda92ed83051747e0bb3e8898639971f1
talvane <talvane.magalhaes@gmail.com>
2020-05-12 15:50:13 -0300

    ajust class Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,46 +2,50 @@ import React from 'react';
 
 import './../styles/Contact.scss';
 
-const Contact = ({ data }) => {
-  const formatDate = new Intl.DateTimeFormat('pt-BR', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-  });
+class Contact extends React.Component {
+  render() {
+    const { data } = this.props;
 
-  const [
-    { value: mm },
-    ,
-    { value: dd },
-    ,
-    { value: yyyy },
-  ] = formatDate.formatToParts(new Date(data.admissionDate));
+    const formatDate = new Intl.DateTimeFormat('pt-BR', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    });
 
-  return (
-    <article className="contact" data-testid="contact">
-      <span className="contact__avatar">
-        <img src={data.avatar} alt="avatar_contact" />
-      </span>
-      <span className="contact__data" data-testid="contact-name">
-        {data.name}
-      </span>
-      <span className="contact__data" data-testid="contact-phone">
-        {data.phone}
-      </span>
-      <span className="contact__data" data-testid="contact-country">
-        {data.country}
-      </span>
-      <span className="contact__data" data-testid="contact-date">
-        {`${yyyy}/${dd}/${mm}`}
-      </span>
-      <span className="contact__data" data-testid="contact-company">
-        {data.company}
-      </span>
-      <span className="contact__data" data-testid="contact-department">
-        {data.department}
-      </span>
-    </article>
-  );
-};
+    const [
+      { value: yyyy },
+      ,
+      { value: mm },
+      ,
+      { value: dd },
+    ] = formatDate.formatToParts(new Date(data.admissionDate));
+
+    return (
+      <article className="contact" data-testid="contact">
+        <span className="contact__avatar">
+          <img src={data.avatar} alt="avatar_contact" />
+        </span>
+        <span className="contact__data" data-testid="contact-name">
+          {data.name}
+        </span>
+        <span className="contact__data" data-testid="contact-phone">
+          {data.phone}
+        </span>
+        <span className="contact__data" data-testid="contact-country">
+          {data.country}
+        </span>
+        <span className="contact__data" data-testid="contact-date">
+          {`${dd}/${mm}/${yyyy}`}
+        </span>
+        <span className="contact__data" data-testid="contact-company">
+          {data.company}
+        </span>
+        <span className="contact__data" data-testid="contact-department">
+          {data.department}
+        </span>
+      </article>
+    );
+  }
+}
 
 export default Contact;
